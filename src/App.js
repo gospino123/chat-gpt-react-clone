da74b2726ea5c95336afd42afd0cbbc114fd5594
gizmo <72733125+gospino123@users.noreply.github.com>
2023-04-29 16:20:34 -0400

    Feat(base,styles): Add,style placeholders

--- src/App.js
+++ src/App.js
@@ -2,16 +2,29 @@ function App() {
   return (
     <div className="App">
       <section className="sidebar">
-        <button>+ New Chat</button>
+        <button id="newChat">+ New Chat</button>
         <ul className="history">
-
+          <li>Hey there</li>
         </ul>
         <nav>
           <p>Made by Will</p>
         </nav>
       </section>
       <section className="main">
-
+        <h1>WillGPT</h1>
+        <ul className="feed">
+        </ul>
+        <div className="bottomSection">
+          <div className="inputContainer">
+            <input id="chatInput"/>
+            <div id="submit">&rarr;</div>
+          </div>
+          <p className="info">
+            Chat GPT Mar 14 Version. Free Research Preview.
+            Our goal is to make AI systems more natural and safe to interact with.
+            Your feedback will help us improve.
+          </p>
+        </div>
       </section>
     </div>
   );
