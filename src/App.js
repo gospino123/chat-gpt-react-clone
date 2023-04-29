8d48cde2eb58714c191c420f3684e6afc3eda369
gizmo <72733125+gospino123@users.noreply.github.com>
2023-04-29 14:31:33 -0400

    Feat(app-structure): Build base sections

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 function App() {
   return (
     <div className="App">
+      <section className="sidebar">
+        <button>+ New Chat</button>
+        <ul className="history">
+
+        </ul>
+        <nav>
+          <p>Made by Will</p>
+        </nav>
+      </section>
+      <section className="main">
+
+      </section>
     </div>
   );
 }
