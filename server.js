5587da7e307214ff648afcff8ef6325d02c1a650
gizmo <72733125+gospino123@users.noreply.github.com>
2023-04-29 21:30:13 -0400

    Feat(api): Set up API call
     -Include error handling and max_tokens 50

--- server.js
+++ server.js
@@ -6,4 +6,28 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const API_KEY = '';
+
+app.post('/completions', async (request, response) => {
+  const options = {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${API_KEY}`,
+      'Content-Type': "application/json"
+    },
+    body: JSON.stringify({
+      model: 'gpt-4',
+      messages: [{role: 'user', content: 'How are you?'}],
+      max_tokens: 50,
+    })
+  }
+  try {
+      const response = await fetch('https://api.openai.com/v1/chat/completions', options);
+      const data = await response.json();
+      response.send(data);
+  } catch (error) {
+      console.error(error);
+  }
+})
+
 app.listen(PORT, () => console.log(`Your server is running on PORT ${PORT}`));
