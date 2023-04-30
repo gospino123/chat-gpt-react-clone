d00f4845ac0956b7bf72246f4a6afc1f2e7a5e23
gizmo <72733125+gospino123@users.noreply.github.com>
2023-04-29 22:51:29 -0400

    Fix(global): Correct API,styles,syntax
     -Keep key secret
     -Change from single to double quotes
     -Change to gpt-3.5-turbo
     -Note: gpt-4 may work still
     -Update post on App.js w getMessages
     -Change cursor to pointer on submit
     -Fix colors

--- server.js
+++ server.js
@@ -6,28 +6,28 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const API_KEY = '';
+const API_KEY = "";
 
-app.post('/completions', async (request, response) => {
+app.post("/completions", async (req, res) => {
   const options = {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Authorization': `Bearer ${API_KEY}`,
-      'Content-Type': "application/json"
+      "Authorization": `Bearer ${API_KEY}`,
+      "Content-Type": "application/json"
     },
     body: JSON.stringify({
-      model: 'gpt-4',
-      messages: [{role: 'user', content: 'How are you?'}],
+      model: "gpt-3.5-turbo",
+      messages: [{ "role": "user", "content": "how are you?"}],
       max_tokens: 50,
     })
   }
   try {
-      const response = await fetch('https://api.openai.com/v1/chat/completions', options);
+      const response = await fetch("https://api.openai.com/v1/chat/completions", options);
       const data = await response.json();
-      response.send(data);
+      res.send(data);
   } catch (error) {
       console.error(error);
   }
-})
+});
 
 app.listen(PORT, () => console.log(`Your server is running on PORT ${PORT}`));
--- src/App.js
+++ src/App.js
@@ -1,4 +1,23 @@
-function App() {
+const App = () => {
+  const getMessages = async () => {
+    const options = {
+      method: 'POST',
+      body: JSON.stringify({
+        message: 'hello how are you?'
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+    try {
+        const response = await fetch('http://localhost:8000/completions', options);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error(error);
+    }
+  }
+
   return (
     <div className="App">
       <section className="sidebar">
@@ -17,7 +36,7 @@ function App() {
         <div className="bottomSection">
           <div className="inputContainer">
             <input id="chatInput"/>
-            <div id="submit">&rarr;</div>
+            <div id="submit" onClick={getMessages}>&rarr;</div>
           </div>
           <p className="info">
             Chat GPT Mar 14 Version. Free Research Preview.
