310ebf6db2a1fd812b5ed282aeb2cd93d3053d60
gizmo <72733125+gospino123@users.noreply.github.com>
2023-05-02 13:25:06 -0400

    Feat(api_key): Store key in server variable

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const API_KEY = "";
+const API_KEY = process.env.API_KEY;
 
 app.post("/completions", async (req, res) => {
   const options = {
